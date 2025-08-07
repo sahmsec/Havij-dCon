c52a336143d6588c59d48d12fa97a861bf5d2915
Sakib Ahmed <141112651+sahmsec@users.noreply.github.com>
2025-08-07 14:29:27 +0600

    Update aws-install.ps1

--- aws-install.ps1
+++ aws-install.ps1
@@ -2,7 +2,7 @@
 [CmdletBinding()]
 param()
 
-$repoBase = "https://raw.githubusercontent.com/sahmsec/Havij-dControl/main"
+$repoBase = "https://raw.githubusercontent.com/sahmsec/havij-dcontrol/main"
 $batUrl = "$repoBase/setup.bat"
 
 # Get desktop path dynamically
